a8b165c8116a99e6775cf3faa3a954081f8dbe5d
masprog2022 <masprog2022@gmail.com>
2023-01-04 19:01:29 +0100

    feat: add repository, service and DTO of User and Role

--- src/main/java/com/devsuperior/dscatalog/entities/User.java
+++ src/main/java/com/devsuperior/dscatalog/entities/User.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -35,7 +36,7 @@ public class User implements Serializable {
 	
 	private String password;
 	
-	@ManyToMany
+	@ManyToMany(fetch = FetchType.EAGER) // permite trazer um usuário do banco com os seus perfis
 	@JoinTable(name = "tb_user_role",
 		joinColumns = @JoinColumn(name = "user_id"),
 		inverseJoinColumns = @JoinColumn(name = "role_id"))	
